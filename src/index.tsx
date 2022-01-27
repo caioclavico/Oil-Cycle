87199edb1d734e5bea9ea2974d138a110043e5fd
Caio Henrique Clavico Crizanto <caiohclavico@gmail.com>
2022-01-27 09:56:30 -0400

    feat: created theme with chakra-ui

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./styles/theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
