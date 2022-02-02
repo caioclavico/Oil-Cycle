37099dc25109213d340a801b777f5016308accb8
Victor Nunes <victornunes@pop-os.localdomain>
2022-02-02 14:32:03 -0300

    feat: conditional render

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,13 +2,20 @@ import { Box } from "@chakra-ui/react";
 import { HeaderDashboard } from "./header-dashboard";
 import { SellerDashboard } from "./seller-dashboard";
 import { BuyerDashboard } from "./buyer-dashboard";
+import { useAuth } from "../../contexts/AuthContext";
 
 export const Dashboard = () => {
+  const { user } = useAuth();
+  let isBuyer = false;
+
+  if ("CNPJ" in user) {
+    isBuyer = true;
+  }
+
   return (
     <Box width="100vw">
       <HeaderDashboard />
-      <SellerDashboard />
-      {/* <BuyerDashboard /> */}
+      {isBuyer ? <BuyerDashboard /> : <SellerDashboard />}
     </Box>
   );
 };
